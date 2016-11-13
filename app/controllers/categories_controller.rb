70d83b5d848223af58372ba0f8be58b403dbf338
Jin C9 <jinchen93@gmail.com>
2016-11-13 20:52:25 +0000

    For categories: add edit pathway on show page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,12 @@
 class CategoriesController < ApplicationController
     before_action :require_admin, except: [:index, :show]
+    before_action :set_category, only: [:edit, :update, :show]
     
     def index
         @categories = Category.paginate(page: params[:page], per_page: 5)
     end
     
     def show
-        @category = Category.find(params[:id])
         @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
     end
     
@@ -24,6 +24,17 @@ class CategoriesController < ApplicationController
         end
     end
     
+    def edit
+    end
+    
+    def update
+        if @category.update(category_params)
+            flash[:success] = "Category name successfully updated"
+            redirect_to categories_path
+        else
+            render 'edit'
+        end
+    end
     
     private
     def category_params
@@ -35,5 +46,9 @@ class CategoriesController < ApplicationController
             flash[:danger] = "Only admins can perform that action"
             redirect_to categories_path
         end
+    end
+    
+    def set_category
+        @category = Category.find(params[:id])
     end
 end
