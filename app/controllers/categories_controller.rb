46982d8b2ceb4cb10dd44bb3268a5a30352a397b
Jin C9 <jinchen93@gmail.com>
2016-11-13 02:23:45 +0000

    Add categories model and tests

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
     def index
-        
+        @categories = Category.paginate(page: params[:page], per_page: 5)
     end
     
     def show
@@ -8,6 +8,22 @@ class CategoriesController < ApplicationController
     end
     
     def new
-        
+        @category = Category.new
+    end
+    
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            flash[:success] = "Category successfully saved"
+            redirect_to categories_path
+        else
+            render 'new'
+        end
+    end
+    
+    
+    private
+    def category_params
+        params.require(:category).permit(:name)
     end
 end
