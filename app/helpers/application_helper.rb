9a1d31c6e0ccfaa742f83a91078e33abc9e9b96c
Jin C9 <jinchen93@gmail.com>
2016-11-16 04:17:47 +0000

    Add titles to all pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,13 @@ module ApplicationHelper
         gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
         image_tag(gravatar_url, alt: user.username, class: 'img-circle')
     end
+    
+    def title(*args)
+        base_title = "Alpha Blog"
+        if args[0]
+            content_for :title, args[0].to_s + " | " + base_title
+        else
+            content_for :title, base_title
+        end
+    end
 end
