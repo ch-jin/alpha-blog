32dbffb19a1cbf3969042be0200f817898a14786
Jin Chen <jinchen93@gmail.com>
2016-11-19 23:58:34 -0800

    Add remember me checkbox

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,18 +8,19 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
         log_in(user)
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
         flash[:success] = "You have successfully logged in"
         redirect_to user_path(user)
     else
         flash.now[:danger] = "There was something wrong with your login information"
-        render 'new'
+        render('new')
     end
   end
 
   def destroy
     log_out
     flash[:success] = "You have logged out"
-    redirect_to root_path
+    redirect_to(root_path)
   end
 
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,14 +1,24 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+
 
   # Add more helper methods to be used by all tests here...
-  
+
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+
   def sign_in_as(user, password)
-    post login_path, session: {email: user.email, password: password}
+   post login_path, session: {email: user.email, password: password}
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post(login_path, params: { session: { email: user.email, password: password,
+                                          remember_me: remember_me } } )
   end
 end
